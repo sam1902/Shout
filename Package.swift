a9f59749fc067a266d2a298bc4f5ed8231282131
Samuel Prevost <samuel.prevost@pm.me>
2021-02-28 12:06:52 +0100

    Add unsafe linker flags

--- Package.swift
+++ Package.swift
@@ -16,7 +16,22 @@ let package = Package(
     ],
     targets: [
         .systemLibrary(name: "CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
-        .target(name: "Shout", dependencies: ["CSSH", "Socket"]),
+        .target(name: "Shout", dependencies: ["CSSH", "Socket"],
+        linkerSettings: [
+                .unsafeFlags(
+                    [
+                        "-I/opt/homebrew/Cellar/openssl@1.1/1.1.1j/include",
+                        "-L/opt/homebrew/Cellar/openssl@1.1/1.1.1j/lib",
+                        "-I/opt/homebrew/Cellar/libssh2/1.9.0_1/include",
+                        "-L/opt/homebrew/Cellar/libssh2/1.9.0_1/lib",
+                        "-lssl",
+                        "-lcrypto",
+                        "-lssh2"
+                    ],
+                    .when(platforms: [.macOS], configuration: .release)
+                ),
+            ]
+        ),
         .testTarget(name: "ShoutTests", dependencies: ["Shout"]),
     ]
 )
